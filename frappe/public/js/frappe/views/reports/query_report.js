166feb1248e76d50d1556b4700cdcef4d2e11ea4
scmmishra <scm.mymail@gmail.com>
2019-04-24 16:07:44 +0530

    fix: syntax error

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -554,7 +554,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 		function get_options(fields) {
 			return fields.map((field) => {
 				return {label: field.label, value: field.fieldname};
-			}
+			});
 		}
 
 		const numeric_fields = columns.filter((col, i) => indices.includes(i));
