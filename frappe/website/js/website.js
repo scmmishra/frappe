07a5666ab69429989780f408acc742e026463361
Shivam Mishra <scm.mymail@gmail.com>
2020-05-22 17:55:00 +0530

    WIP

--- frappe/website/js/website.js
+++ frappe/website/js/website.js
@@ -454,19 +454,8 @@ $(document).on("page-change", function() {
 
 });
 
-
-frappe.ready(function() {
-	frappe.call({
-		method: 'frappe.website.doctype.website_settings.website_settings.is_chat_enabled',
-		callback: (r) => {
-			if (r.message) {
-				frappe.require(['/assets/js/moment-bundle.min.js', "/assets/css/frappe-chat-web.css", "/assets/frappe/js/lib/socket.io.min.js"], () => {
-					frappe.require('/assets/js/chat.js', () => {
-						frappe.chat.setup();
-					});
-				});
-			}
-		}
+frappe.require(["/assets/css/frappe-chat-web.css"], () => {
+	frappe.require('/assets/js/chat.js', () => {
+		frappe.chat.setup();
 	});
-	frappe.socketio.init(window.socketio_port);
 });
