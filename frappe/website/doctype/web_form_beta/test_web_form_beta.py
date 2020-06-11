dec14b799082af4bf529e146f0bbeae05fc49040
Shivam Mishra <scm.mymail@gmail.com>
2020-06-11 16:35:50 +0530

    feat: added route controller

--- frappe/website/doctype/web_form_beta/test_web_form_beta.py
+++ frappe/website/doctype/web_form_beta/test_web_form_beta.py
@@ -2,9 +2,30 @@
 # Copyright (c) 2019, Frappe Technologies and Contributors
 # See license.txt
 from __future__ import unicode_literals
-
-# import frappe
+import frappe
 import unittest
 
 class TestWebFormBeta(unittest.TestCase):
-	pass
+	def setUp(self):
+		frappe.conf.disable_website_cache = True
+		frappe.local.path = None
+
+	def tearDown(self):
+		frappe.conf.disable_website_cache = False
+		frappe.local.path = None
+	
+	def test_make_route(self):
+		test_doc = get_test_doc()
+		# Test no route
+		self.assertEqual(test_doc.make_route(), "form/-test-web-form-beta")
+		
+		# Test dirty routes
+		test_routes = ["//hello", "form/hello", "/form///hello//", "form/hello//"]
+		for route in test_routes:
+			test_doc.route = route
+			self.assertEqual(test_doc.make_route(), "form/hello")
+
+def get_test_doc():
+	doc = frappe.new_doc("Web Form Beta")
+	doc.title = "_Test Web Form Beta"
+	return doc
--- frappe/website/doctype/web_form_beta/web_form_beta.py
+++ frappe/website/doctype/web_form_beta/web_form_beta.py
@@ -4,12 +4,38 @@
 
 from __future__ import unicode_literals
 import frappe
+import re
 from frappe.website.website_generator import WebsiteGenerator
 
 class WebFormBeta(WebsiteGenerator):
 	website = frappe._dict(
 		no_cache = 1
 	)
+
+	def validate(self):
+		self.route = self.make_route()
+
+	def make_route(self):
+		"""Generate or prefix route
+
+		Returns:
+			string: route with 'form/' prefix
+		"""	
+		prefix = "form/"
+		if not self.route:
+			# If not route is present, scrub the title
+			self.route = prefix + self.scrub(self.title)
+		else:
+			# remove trailing and leading slashes from route
+			self.route = self.route.strip('/')
+			
+		if not self.route.startswith('form/'):
+			# If route does not start with form, add it
+			# strip leading / from route
+			self.route = prefix + self.route.lstrip('/')
+
+		# replace multiple slashes with single
+		return re.sub(r'[/]+', '/', self.route)
 
 
 def get_fields():
